2ec03786454444a55822ae9f68b79b80ffc26829
Henrique L S Silva <lsshenrique@hotmail.com>
2020-03-24 19:49:50 -0300

    add slot in validSelect

--- src/components/ValidSelect.vue
+++ src/components/ValidSelect.vue
@@ -28,7 +28,26 @@
             :smallChips="smallChips"
             v-bind="$attrs"
             v-on="$listeners"
-        />
+         >
+            <!-- Pass on all named slots -->
+            <slot
+                v-for="slot in Object.keys($slots)"
+                :name="slot"
+                :slot="slot"
+            />
+
+            <!-- Pass on all scoped slots -->
+            <template
+                v-for="slot in Object.keys($scopedSlots)"
+                :slot="slot"
+                slot-scope="scope"
+            >
+                <slot
+                    :name="slot"
+                    v-bind="scope"
+                />
+            </template>
+        </v-select>
     </ValidationProvider>
 </template>
 
